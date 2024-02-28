4bafe075dba110a5ef046467ab4923ef5369a5d9
Suruthi-Kannan <suruthikannan24@gmail.com>
2024-02-29 01:48:34 +0530

    Initial-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes,Route} from 'react-router-dom';
+import Body from  "./Body.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="relative font-inter antialiased">
+        <div class="text-center text-2xl pr-8 pl-8 pt-8 text-gray-900 ">
+          Choose the best plan for your bussiness
+        </div>
+        <Routes>
+          <Route path="/" element={<Body/>}/> 
+        </Routes>
+      
     </div>
   );
 }
